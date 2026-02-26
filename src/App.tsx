ffa4d99ee42a9bba14ea42dbf943ad2ed815427d
Alonso <alonsonh94@gmail.com>
2026-02-25 21:01:38 -0600

    feat: Add Sass, SCSS module and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { Button } from "antd";
-import "./App.css";
+import React from 'react';
+import styles from './App.module.scss';
+import { Button, Layout } from 'antd';
 
-function App() {
-  const title = "Template Web React";
+const { Header, Footer, Content } = Layout;
 
+const App: React.FC = () => {
   return (
-    <>
-      <h1>{title}</h1>
-      <p> This is a base web react</p>
-      <Button type="primary">Click me</Button>
-    </>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ backgroundColor: '#001529', color: 'white', textAlign: 'center', fontSize: '1.5rem' }}>
+        Welcome to Vite + React + AntD
+      </Header>
+      <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div className={styles.hello}>Hello World!</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Powered by React, TypeScript, Vite, and Ant Design
+        <br />
+        <Button type="primary" style={{ marginTop: '1rem' }}>Learn More</Button>
+      </Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
