277bfdaf822136bc8ea5944677bb11f3c766ae7d
Alonso <alonsonh94@gmail.com>
2026-04-22 20:56:48 -0600

    chore: Add env files and Vite env typings

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENV?: "local" | "dev" | "prod";
+  readonly VITE_BASE_API_URL?: string;
+  readonly VITE_API_VERSION?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
